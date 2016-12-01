a46024026e105d63aa4b2cd45a1e43bb649d99e9
Javier E. Perez P <javier.perez.puello@gmail.com>
2016-12-01 02:02:44 -0400

    Making keys order more friendly
    Simulate real keyboard position

--- t9-to-multitap.rb
+++ t9-to-multitap.rb
@@ -1,15 +1,10 @@
 #!/usr/bin/ruby
 
-keyboard = { 1 => [1],
-    2 => ['a', 'b', 'c'],
-    3 => %w[ d e f   ],
-    4 => %w[ g h i   ],
-    5 => %w[ j k l   ],
-    6 => %w[ m n o   ],
-    7 => %w[ p q r s ],
-    8 => %w[ t u v   ],
-    9 => %w[ w x y z ],
-    0 => [" "]
+keyboard = {
+  1 => [1],           2 => %w[ a b c ], 3 => %w[ d e f   ],
+  4 => %w[ g h i   ], 5 => %w[ j k l ], 6 => %w[ m n o   ],
+  7 => %w[ p q r s ], 8 => %w[ t u v ], 9 => %w[ w x y z ],
+                      0 => [" "]
 }
 
 word = 'github dvst'
